418aaad9f35b67655c229a3ba759c3f6c3c78989
Yanisa w <mods.loan.assistant@gmail.com>
2021-11-09 04:06:48 +0700

    cpnLogOn by J2

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 const firebaseConfig = firebase.initializeApp({
     apiKey: "AIzaSyD_PU5bw4-vNEYL5ew7sow970gTu-ZtLG4",
